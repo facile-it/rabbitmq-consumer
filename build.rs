8c00c87e1d539218494bfc606c1272ac293f2b6c
Emulator000 <emulator@hotmail.it>
2020-10-19 01:53:27 +0200

    Trying a final fix for Travis CI

--- build.rs
+++ build.rs
@@ -1,7 +1,11 @@
+use std::env;
+
 fn main() {
-    println!(r"cargo:rustc-link-lib=static=mysqlclient");
-    println!(r"cargo:rustc-link-lib=static-nobundle=stdc++");
-    println!(r"cargo:rustc-link-lib=static=z");
-    println!(r"cargo:rustc-link-lib=static=ssl");
-    println!(r"cargo:rustc-link-lib=static=crypto");
+    if "release" == env::var("PROFILE").unwrap_or("".into()) {
+        println!(r"cargo:rustc-link-lib=static=mysqlclient");
+        println!(r"cargo:rustc-link-lib=static-nobundle=stdc++");
+        println!(r"cargo:rustc-link-lib=static=z");
+        println!(r"cargo:rustc-link-lib=static=ssl");
+        println!(r"cargo:rustc-link-lib=static=crypto");
+    }
 }
